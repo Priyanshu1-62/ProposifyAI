77732b94a21e9647b42c476a163cd426852d3796
Priyanshu1-62 <bpriyanshu5678@gmail.com>
2026-01-18 00:05:16 +0530

    Set Rtk store and provider wrapper

---
+++ client/src/features/requestPipeline/requestSlice.ts
@@ -0,0 +1,33 @@
+// import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+// const initialState = {
+//     request: [
+//         {
+//             id: 1
+//         }
+//     ]
+// }
+
+// async function getRequestProfile(){
+//     try {
+        
+//     } 
+//     catch (error) {
+//         throw error;
+//     }
+// }
+
+// export const requestSlice = createSlice({
+//     name: "request",
+//     initialState,
+//     reducers: {
+//         getRequestProfile: getRequestProfile,
+//         addRequest: (state, action) => {
+
+//         }
+//     }
+// });
+
+// export { getRequestProfile, addRequest } = requestSlice.actions;
+
+// export default requestSlice.reducer;
