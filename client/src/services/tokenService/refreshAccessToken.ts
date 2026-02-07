a096980a9d2a316dae3814fbed9945ab97e68d07
Priyanshu1-62 <bpriyanshu5678@gmail.com>
2026-02-07 18:37:19 +0530

    Update refresh token API route

--- client/src/services/tokenService/refreshAccessToken.ts
+++ client/src/services/tokenService/refreshAccessToken.ts
@@ -2,7 +2,7 @@ const apiURL = import.meta.env.VITE_API_URL;
 
 export async function refreshAccessToken(){
     try {
-        const response = await fetch(`${apiURL}/auth/token/refresh`);
+        const response = await fetch(`${apiURL}/api/auth/token/refresh`);
         if(response.ok){
             const data = await response.json();
             localStorage.setItem("accessToken", data.accessToken); 
