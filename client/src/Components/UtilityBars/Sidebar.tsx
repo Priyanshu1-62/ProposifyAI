c4df5948f12fe69a792c830815bc8ead03550350
Priyanshu1-62 <bpriyanshu5678@gmail.com>
2026-03-08 00:00:02 +0530

    Minor UI imporvement

--- client/src/Components/UtilityBars/Sidebar.tsx
+++ client/src/Components/UtilityBars/Sidebar.tsx
@@ -10,7 +10,7 @@ import alertContext from '../../Contexts/alertContext';
 
 function Sidebar() {
   const navigate = useNavigate();
-  const { sidebar } = useContext(userContext)!;
+  const { sidebar, setSidebar } = useContext(userContext)!;
   const { handleApiResponse } = useContext(alertContext)!;
 
   const handleLogout = async () => {
@@ -19,21 +19,41 @@ function Sidebar() {
     handleApiResponse(result, "Log Out successful !!");
     navigate("/", { replace: true });
   }
+
+  const navigateCreateRequest = () => {
+    setSidebar(false);
+    navigate("/request/title");
+  }
+
+  const navigateRequests = () => {
+    setSidebar(false);
+    navigate("/requests");
+  }
+
+  const navigateCreateRespondentGroup = () => {
+    setSidebar(false);
+    navigate("/respondentGroup/create");
+  }
+
+  const navigateRespondentGroups = () => {
+    setSidebar(false);
+    navigate("/respondentGroups");
+  }
   return (
     <>
     <div className={`z-2 absolute top-[6.6vh] w-60 h-[93.4vh] flex flex-col justify-between py-2 px-2 text-xs text-white bg-[#3c3744] border-r-2 border-[#564f62] ${sidebar ? "translate-x-0" : "-translate-x-full"} transition-all ease-in-out duration-200 delay-100 shadow-xl shadow-stone-900`}>
       <ul className="flex flex-col gap-2 mt-2">
         <li>
-          <button className="w-full flex gap-2 items-center px-2 py-2 hover:bg-[#686274] active:bg-[#797385] hover:cursor-pointer rounded-xl transition-all delay-75" onClick={()=>{navigate("/request/title")}}><FaPlus size={18}/> Create Request</button>
+          <button className="w-full flex gap-2 items-center px-2 py-2 hover:bg-[#686274] active:bg-[#797385] hover:cursor-pointer rounded-xl transition-all delay-75" onClick={navigateCreateRequest}><FaPlus size={18}/> Create Request</button>
         </li>
         <li>
-          <button className="w-full flex gap-2 items-center px-2 py-2 hover:bg-[#686274] active:bg-[#797385] hover:cursor-pointer rounded-xl transition-all delay-75" onClick={()=>{navigate("/requests")}}><GrTasks size={18}/> My Requests</button>
+          <button className="w-full flex gap-2 items-center px-2 py-2 hover:bg-[#686274] active:bg-[#797385] hover:cursor-pointer rounded-xl transition-all delay-75" onClick={navigateRequests}><GrTasks size={18}/> My Requests</button>
         </li>
         <li>
-          <button className="w-full flex gap-2 items-center px-2 py-2 hover:bg-[#686274] active:bg-[#797385] hover:cursor-pointer rounded-xl transition-all delay-75" onClick={()=>{navigate("/respondentGroup/create")}}><FaPlus size={18}/> Add Respondent group</button>
+          <button className="w-full flex gap-2 items-center px-2 py-2 hover:bg-[#686274] active:bg-[#797385] hover:cursor-pointer rounded-xl transition-all delay-75" onClick={navigateCreateRespondentGroup}><FaPlus size={18}/> Add Respondent group</button>
         </li>
         <li>
-          <button className="w-full flex gap-2 items-center px-2 py-2 hover:bg-[#686274] active:bg-[#797385] hover:cursor-pointer rounded-xl transition-all delay-75" onClick={()=>{navigate("/respondentGroups")}}><FaPeopleGroup size={18}/> My Respondent groups</button>
+          <button className="w-full flex gap-2 items-center px-2 py-2 hover:bg-[#686274] active:bg-[#797385] hover:cursor-pointer rounded-xl transition-all delay-75" onClick={navigateRespondentGroups}><FaPeopleGroup size={18}/> My Respondent groups</button>
         </li>
       </ul>
       <div className='w-full flex gap-4 justify-around items-center px-2 py-2 hover:bg-[#686274] active:bg-[#797385] hover:cursor-pointer rounded-xl transition-all delay-75' onClick={handleLogout}>
